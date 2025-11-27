b8c7c6724971d2c90856c431338c576acd978d6c
Akhil Kaladharan <akhilk1947@gmail.com>
2025-11-27 20:11:09 +0530

    Solved the issue of nit calicut sentering in app

--- app/src/main/java/com/example/nit_calciut_bus_demo/ui/theme/BusStopsFragment.kt
+++ app/src/main/java/com/example/nit_calciut_bus_demo/ui/theme/BusStopsFragment.kt
@@ -20,17 +20,7 @@ class BusStopsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
-    companion object {
-        // Bus stops coordinates
-        private val southCampus = LatLng(11.314111, 75.932000)
-        private val somsHostel = LatLng(11.314833, 75.932417)
-        private val lhHostel = LatLng(11.318333, 75.931028)
-        private val eclhc = LatLng(11.323056, 75.937278)
-        private val architecture = LatLng(11.322944, 75.936444)
-        private val deptBuilding = LatLng(11.321639, 75.935028)
-        private val centerCircle = LatLng(11.321500, 75.934167)
-        private val megaHostel = LatLng(11.317167, 75.937667)
-    }
+    companion object {}
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -65,19 +55,25 @@ class BusStopsFragment : Fragment(), OnMapReadyCallback {
     // Try to load from backend using college code; if it fails, show an error toast
     loadAndRenderStops()
 
-        // Center the map
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerCircle, 15f))
+        // Camera will be centered to fetched stops when available
     }
 
     private fun addBusStopMarkers(stops: List<Pair<LatLng, String>>) {
         val busIcon = getScaledBusIcon(28)
 
+        val boundsBuilder = LatLngBounds.Builder()
         stops.forEach { (position, title) ->
             mMap.addMarker(MarkerOptions()
                 .position(position)
                 .title(title)
                 .icon(busIcon)
                 .anchor(0.5f, 0.5f))
+            boundsBuilder.include(position)
+        }
+        if (stops.isNotEmpty()) {
+            val bounds = boundsBuilder.build()
+            val padding = 100
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, padding))
         }
     }
 
